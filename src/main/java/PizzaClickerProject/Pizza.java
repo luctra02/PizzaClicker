1a822c88825ffa8e97c44eba08abc53e671d1cc2
Lucas Tran <lucastra@stud.ntnu.no>
2022-03-08 17:15:39 +0100

    noen endringer

--- src/main/java/PizzaClickerProject/Pizza.java
+++ src/main/java/PizzaClickerProject/Pizza.java
@@ -5,8 +5,9 @@ import java.util.Arrays;
 
 public class Pizza {
     private List <String> pizzaTypes = Arrays.asList("First Price", "Rema 1000", "Coop", "Folkets","Dr.Oetker","Big One","Grandiosa");
+    private List <Integer> pizzaValues = Arrays.asList(0,1000,5000,10000,30000,50000,100000);
     private String pizzaType = pizzaTypes.get(0);
-    private int pizzaValue;
+    private int pizzaValue = pizzaValues.get(0);
 
     
 }
