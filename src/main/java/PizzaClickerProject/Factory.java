02d4e53d72a1b102f686507e8ebc40e4ff460cda
Mads Hermansen <mdherman@stud.ntnu.no>
2022-04-28 16:53:02 +0200

    feck lucas

--- src/main/java/PizzaClickerProject/Factory.java
+++ src/main/java/PizzaClickerProject/Factory.java
@@ -13,7 +13,7 @@ public class Factory {
     private Pizza pizza;
     // private Worker worker;
     private ArrayList<Worker> workerObjects = new ArrayList<>();
-    private double currentBalance = 1000000;
+    private double currentBalance = 0;
     private ArrayList<String> workerTypes = new ArrayList<>(Arrays.asList("Deliverer","Cashier","Chef","Manager","CEO"));
 
     
@@ -23,7 +23,7 @@ public class Factory {
         if(!factoryName.matches("[A-z-0-9]{6,20}")){
             throw new IllegalArgumentException("Invalid username! Make sure the name is between 6-20 characters from a-z.");
         }
-        if(factoryName == "lucaspai"){
+        if(factoryName == "admin"){
             currentBalance = 1000000;
         }
         this.factoryName = factoryName;
---
+++ src/test/java/PizzaClicker/FactoryTest.java
@@ -0,0 +1,42 @@
+package PizzaClicker;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import PizzaClickerProject.Factory;
+
+
+public class FactoryTest {
+    private Factory factory;
+
+    @BeforeEach
+    public void setup() {
+        factory = new Factory("creampie");
+
+    }
+
+    @Test
+	@DisplayName("Tester om validering av brukernavn")
+    public void testFactoryName(){
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Factory("test");
+        }, "IllegalArgumentException skal utløses når navnet er under 6 bokstaver");
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Factory("testover20digitslmaopoghahaha");
+        }, "IllegalArgumentException skal utløses når navnet er over 20 bokstaver");
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Factory("ÅÅØÆÅØÅ");
+        }, "IllegalArgumentException skal utløses når brukernavnet har andre bokstaver enn A-Z");
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Factory("-.,@^¨`");
+        }, "IllegalArgumentException skal utløses når navnet har symboler");
+    }
+
+	
+
+    
+}
